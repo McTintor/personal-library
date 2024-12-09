7883c17c70879cb33d22f51b1824c6d908711798
McTintor <igortintor99@gmail.com>
2024-12-09 12:04:27 +0100

    added middleware function to book routes

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -6,11 +6,11 @@ const { ensureAuthenticated } = require('../middlewares/authMiddleware');
 // Books routes
 router.get('/mainpage', ensureAuthenticated, bookController.getBooks);
 router.get('/mainpage/:genre', ensureAuthenticated, bookController.getBooksByGenre);
-router.get('/new', bookController.getAddForm);
+router.get('/new', ensureAuthenticated, bookController.getAddForm);
 router.post('/new', ensureAuthenticated, bookController.addBook);
-router.get('/details/:id', bookController.getBookById);
-router.post('/delete/:id', bookController.deleteBook);
-router.get('/edit/:id', bookController.getEditForm);
-router.post('/edit/:id', bookController.editBook);
+router.get('/details/:id', ensureAuthenticated, bookController.getBookById);
+router.post('/delete/:id', ensureAuthenticated, bookController.deleteBook);
+router.get('/edit/:id', ensureAuthenticated, bookController.getEditForm);
+router.post('/edit/:id', ensureAuthenticated, bookController.editBook);
 
 module.exports = router;
